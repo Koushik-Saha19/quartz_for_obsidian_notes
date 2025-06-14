a38e2f8b1a61c39b291b7f8a3d5249da8f9cc1c3
SAHA <ksaha6@dxc.com>
2025-06-15 00:01:40 +0530

    adding files

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "My notes",
+    pageTitle: "My Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://Koushik-Saha19.github.io/my_devops_notes",
+    baseUrl: "https://koushik-saha19.github.io/devops_notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
